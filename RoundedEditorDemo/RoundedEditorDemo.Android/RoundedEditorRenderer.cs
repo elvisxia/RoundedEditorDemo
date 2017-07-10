bd5fc45de52f49e220a3633956bf823a81e2b340
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <v-xucxia@microsoft.com>
2017-07-10 10:27:57 +0800

    Add project files.

---
+++ RoundedEditorDemo/RoundedEditorDemo.Android/RoundedEditorRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using RoundedEditorDemo;
+using RoundedEditorDemo.Droid;
+
+[assembly:ExportRenderer(typeof(RoundedEditor),typeof(RoundedEditorRenderer))]
+namespace RoundedEditorDemo.Droid
+{
+    public class RoundedEditorRenderer:EditorRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = Xamarin.Forms.Forms.Context.GetDrawable(Resource.Drawable.RoundedEditText);
+            }
+        }
+    }
+}
